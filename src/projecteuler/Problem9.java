ca279275fe0913388095708137d10646e59f7c37
SneezingNapkin <sandalap@gmail.com>
2016-03-29 01:15:17 +0100

    Started Problem 9.

---
+++ src/projecteuler/Problem9.java
@@ -0,0 +1,27 @@
+
+package projecteuler;
+
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+import java.util.ArrayList;
+
+public class Problem9 {
+    
+    public static void main(String[] args) {
+        /* I calculated that: 
+            1 < a < 332
+            2 < b < 499
+            334 < c < 997
+        */
+        
+        int c = 334;
+        ArrayList primeList = new ArrayList();
+        
+    }
+    
+}
