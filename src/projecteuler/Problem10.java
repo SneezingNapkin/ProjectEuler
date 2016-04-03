2801b300589aed811bef010102c4656516c75697
SneezingNapkin <sandalap@gmail.com>
2016-04-03 02:09:47 +0100

    Problem 10 now calls the Primes method rather than repeating the same code for finding primes.

--- src/projecteuler/Problem10.java
+++ src/projecteuler/Problem10.java
@@ -5,30 +5,19 @@ package projecteuler;
 Find the sum of all the primes below two million.
 */
 
-import java.util.*;
-
 public class Problem10 {
-    
+    /*
+    I used the "Primes" method to solve this problem making the code very
+    short for this class.
+    */
      public static void main(String[] args) {
-        int NUMBER = 2000000; //set what number we want to look for primes until.
-        long sumOfPrimeNumbers = 5L; // 2 and 3 already included.
-        long count = 1L;
-
-        while (count < NUMBER){
-            for (int i = 2; i <= Math.sqrt(count); i++){
-                //System.out.println("i = " + i);
-                if (count % i == 0){
-                    //System.out.println("i divided the number:" + i + " divided " + count);
-                    break;
-                }
-                else if (i == Math.floor(Math.sqrt(count)) && count % i != 0){
-                    //System.out.println("Number [" + count + "] is prime");
-                    sumOfPrimeNumbers += count;
-                }
-            }
-            count += 2;
-        }
+        Primes arrayOfPrimes = new Primes();
+        arrayOfPrimes.LIMIT = 2000000; // setting the limit
         
-        System.out.println("The sum of the prime numbers is: "+ sumOfPrimeNumbers);
+        // Calling the method to solve for the limit that was set:
+        arrayOfPrimes.Solve();
+        
+        // calling the sum of primes that were just generated:
+        System.out.println("2nd method: The sum of the prime numbers is: "+ arrayOfPrimes.sum);
     }
 }
